402bb502e868eeb091d307fabb445c9d70a7ac7a
Onur Kesici <onurkesici0@gmail.com>
2022-11-15 20:29:02 +0300

    last update

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,9 +1,72 @@
-import "./profile.scss"
+import "./profile.scss";
+import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PlaceIcon from "@mui/icons-material/Place";
+import LanguageIcon from "@mui/icons-material/Language";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Posts from "../../components/posts/Posts"
 
 const Profile = () => {
   return (
-    <div className="profile">Profile</div>
-  )
-}
+    <div className="profile">
+      <div className="images">
+        <img
+          src="https://images.pexels.com/photos/12197163/pexels-photo-12197163.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+          className="cover"
+        />
+        <img
+          src="https://imgs.search.brave.com/1_hL-ApqVzwyxAqZYoUO93zkLNZLHiO-zgz1hC-j_Mg/rs:fit:280:280:1/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vMjgw/eDI4MF9SUy9mMy85/Yy81My9mMzljNTMy/NDUwMWE3YmVlNTQ5/NGI2N2E0Zjk1NjU5/Ni5qcGc"
+          alt=""
+          className="profilePic"
+        />
+      </div>
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+            <a href="http://facebook.com">
+              <FacebookTwoToneIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <InstagramIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <TwitterIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <LinkedInIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <PinterestIcon fontSize="large" />
+            </a>
+          </div>
+          <div className="center">
+            <span>Büşra Kesici</span>
+            <div className="info">
+              <div className="item">
+                <PlaceIcon />
+                <span>TR</span>
+              </div>
+              <div className="item">
+                <LanguageIcon />
+                <span>kesici.dev</span>
+              </div>
+            </div>
+            <button>follow</button>
+          </div>
+          <div className="right">
+            <EmailOutlinedIcon />
+            <MoreVertIcon />
+          </div>
+        </div>
+      <Posts/>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
